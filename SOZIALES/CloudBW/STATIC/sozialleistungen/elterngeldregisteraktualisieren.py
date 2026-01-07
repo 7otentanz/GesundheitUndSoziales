0290e5d7cf60202f7f54f88af9a5ceac49054a68
lou-pf <pfefferkorn_laura@teams.hs-ludwigsburg.de>
2026-01-07 20:06:25 +0100

    Alles größer 1 Jahr in Elterngeld wird aus dem Register entfehrt

--- SOZIALES/CloudBW/STATIC/sozialleistungen/elterngeldregisteraktualisieren.py
+++ SOZIALES/CloudBW/STATIC/sozialleistungen/elterngeldregisteraktualisieren.py
@@ -12,47 +12,32 @@ def elterngeldsync():
 
     with open(f"{static_soz}/elterngeld.json", "r", encoding="utf-8") as datei:
         elterngeldregister = json.load(datei)
-        print (elterngeldregister)
+        #print (elterngeldregister)
 
     #Eltern noch nicht in Elterngeldbereichtigte.json
     heute = datetime.date.today()
     ein_jahr = heute - relativedelta(years=1)
+    print (f"heute:  {heute}    ein Jahr: {ein_jahr}" ) 
     
 
     #soll nur Datum prüfen --> wenn
 
-    for berechtigte in elterngeldregister["berechtigte"]:
-        for person_id, datum_str in berechtigte.items():
-            eintrag_datum = datetime.datetime.strptime(datum_str, "%Y-%m-%d").date()
-            print (eintrag_datum)
+    for berechtigte in list(elterngeldregister["berechtigte"]):
+        eintrag_datum = datetime.datetime.strptime(berechtigte["datum"], '%Y-%m-%d').date()
+         
+        print (eintrag_datum)
 
-        if heute - eintrag_datum > ein_jahr:
+        if eintrag_datum < ein_jahr:
             elterngeldregister["berechtigte"].remove(berechtigte)
 
 
-
-
-    
-    # if id_sorgebrechtigter1 not in list(elterngeldregister["berechtigte"]):
-    #     elterngeldregister["berechtigte"].append({"id_sorgebrechtigter1" : id_sorgebrechtigter1, "Datum" : datum})
-    # if id_sorgebrechtigter2 not in list(elterngeldregister["berechtigte"]):
-    #     elterngeldregister["berechtigte"].append({"id_sorgebrechtigter2" : id_sorgebrechtigter2, "Datum" : datum})
-                
-    # if id_sorgebrechtigter1 in list(elterngeldregister["berechtigte"]):
-    #     for eintrag in elterngeldregister["berechtigte"]:
-    #         eintrag["Datum"] = datum
-
-
-    #Eltern bereits in Elterngeldregister.json --> neues Kind, aktualisierung Datum
-
-
-    
 
     with open(f"{static_soz}/elterngeld.json", "w", encoding="utf-8") as datei:
         json.dump(elterngeldregister, datei, indent=4)
 
     with open(f"{static_soz}/elterngeldaktualisierung.txt", "w") as datei:
         datei.write(f"Elterngeldregister aktualisiert am {datetime.datetime.now()}.")
+        print(elterngeldregister)
     
 #        return HttpResponse("OK", status=200)
     
