809b452f05123850c9904849229166fee55948e1
Tim Hauser <t_hauser@web.de>
2026-01-10 16:29:46 +0100

    Schichten für Ärzte hinzugefügt. Wöchentliche Rotation eingebaut... wie der allerletzte Bauer.

--- GESUNDHEIT/CloudBW/STATIC/arztregisteraktualisieren.py
+++ GESUNDHEIT/CloudBW/STATIC/arztregisteraktualisieren.py
@@ -6,6 +6,7 @@ import datetime
 static = "/var/www/static"
 
 spezialisierungen = ["Neurologe", "Pulmologe", "Kardiologe", "Zahnarzt", "Dentalchirurg", "Unfallchirurg", "Allgemeinmediziner", "Gynäkologe", "Psychiater"]
+schichten = ["Früh", "Spät", "Nacht", "Frei"]
 
 def arztregistrieren():
 
@@ -43,6 +44,51 @@ def arztregistrieren():
             continue #Platzhalter
     
     print(arztregister)
+
+    # Listen mit Standorten anlegen als Grundlage für die Schichtverteilung
+    standorte = dict()
+    for arzt in arztregister["personen"]:
+        standort = arzt["standort"]
+        if standort not in standorte:
+            standorte[standort] = list()
+        standorte[standort].append(arzt)
+
+    for standort, aerzte in standorte.items():
+
+        frueh = 0
+        spaet = 0
+        nacht = 0
+        frei = 0
+
+        # Rotation nach Kalenderwoche
+        kalenderwoche = datetime.date.today().isocalendar().week
+
+        if kalenderwoche in [1, 5, 9, 13, 17, 21, 25, 29, 33, 37, 41, 45, 49]:
+            frueh += 1
+        if kalenderwoche in [2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50]:
+            spaet += 1
+        if kalenderwoche in [3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51]:
+            nacht += 1
+        if kalenderwoche in [4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52]:
+            frei += 1
+
+        for arzt in aerzte:
+
+            if frueh <= spaet and frueh <= nacht and frueh <= frei:
+                arzt["schicht"] = "früh"
+                frueh += 1
+            
+            elif spaet <= frueh and spaet <= nacht and spaet <= frei:
+                arzt["schicht"] = "spät"
+                spaet += 1
+
+            elif nacht <= frueh and nacht <= spaet and nacht <= frei:
+                arzt["schicht"] = "nacht"
+                nacht += 1
+
+            else:
+                arzt["schicht"] = frei
+                frei += 1
 
     with open(f"{static}/arztregister.json", "w", encoding="utf-8") as datei:
         json.dump(arztregister, datei, indent=4)
